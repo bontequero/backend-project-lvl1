f70f2dd76cd28e6b41a08f2b30b13e9e09dee73f
bontequero <bontequero@gmail.com>
2020-09-10 00:17:23 +0300

    add brain-calc game

---
+++ bin/brain-calc.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { runGame } from '../src/index.js';
+import calc from '../src/games/brain-calc.js';
+
+runGame(calc);
---
+++ src/games/brain-calc.js
@@ -0,0 +1,29 @@
+import { getRandomInt } from '../index.js';
+
+const minNumber = 0;
+const maxNumber = 30;
+
+const operators = ['+', '-', '*'];
+const computeMap = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
+
+export default {
+  rules: () => 'What is the result of the expression?',
+  question: () => {
+    const a = getRandomInt(minNumber, maxNumber);
+    const b = getRandomInt(minNumber, maxNumber);
+    const op = operators[getRandomInt(0, 3)];
+    const printed = `${a} ${op} ${b}`;
+
+    const computeFn = computeMap[op];
+    const answer = computeFn(a, b);
+
+    return {
+      string: printed,
+      answer: `${answer}`,
+    };
+  },
+};
