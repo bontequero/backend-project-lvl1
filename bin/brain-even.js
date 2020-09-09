e5655478f13e938948dfe3e629b25de91cdbe474
bontequero <bontequero@gmail.com>
2020-09-10 00:04:23 +0300

    fix lint errors

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import { runGame } from '../src/index.js';
-import { braineven } from '../src/games/brain-even.js';
+import braineven from '../src/games/brain-even.js';
 
 runGame(braineven);
--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,14 +3,14 @@ import { getRandomInt } from '../index.js';
 const minNumber = 0;
 const maxNumber = 100;
 
-export const braineven = {
+export default {
   rules: () => 'Answer "yes" if the number is even, otherwise answer "no".',
   question: () => {
     const number = getRandomInt(minNumber, maxNumber);
     const answer = number % 2 === 0 ? 'yes' : 'no';
     return {
       string: number,
-      answer: answer
-    }
-  }
+      answer,
+    };
+  },
 };
