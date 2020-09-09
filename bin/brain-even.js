56e59b20a890e62495d14dc63fbc35f86ad8a7ae
bontequero <bontequero@gmail.com>
2020-09-10 00:01:11 +0300

    extract game engine

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,58 +1,6 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import { runGame } from '../src/index.js';
+import { braineven } from '../src/games/brain-even.js';
 
-const answersToWin = 3;
-const minNumber = 0;
-const maxNumber = 100;
-
-const isEven = (num) => num % 2 === 0;
-
-const getRandomInt = (minp, maxp) => {
-  const min = Math.ceil(minp);
-  const max = Math.floor(maxp);
-  return Math.floor(Math.random() * (max - min) + min);
-};
-
-const step = () => {
-  const number = getRandomInt(minNumber, maxNumber);
-  const question = `Question: ${number}`;
-  console.log(question);
-
-  const answer = readlineSync.question('Your answer: ');
-
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-
-  const isCorrect = answer === correctAnswer;
-  if (!isCorrect) {
-    const tip = `"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`;
-    console.log(tip);
-    return false;
-  }
-
-  console.log('Correct!');
-  return true;
-};
-
-const runGame = (num) => {
-  console.log('Welcome to the Brain Games!');
-
-  const name = readlineSync.question('May I have your name? ');
-  const welcomeMessage = `Hello, ${name}!`;
-  console.log(welcomeMessage);
-
-  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
-  console.log(rules);
-
-  for (let i = 1; i <= num; i += 1) {
-    const isCorrect = step();
-    if (!isCorrect) {
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  }
-
-  console.log(`Congratulations, ${name}!`);
-};
-
-runGame(answersToWin);
+runGame(braineven);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
-#!/usr/bin/env node
-
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+const maxCorrectAnswers = 3;
 
-const actual = readlineSync.question('May I have your name? ');
+export const getRandomInt = (minp, maxp) => {
+  const min = Math.ceil(minp);
+  const max = Math.floor(maxp);
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
-const welcomeMessge = `Hello, ${actual}!`;
+export const runGame = (game) => {
+  console.log('Welcome to the Brain Games!');
 
-console.log(welcomeMessge);
+  const name = readlineSync.question('May I have your name? ');
+  const welcomeMessage = `Hello, ${name}!`;
+  console.log(welcomeMessage);
+
+  const rules = game.rules();
+  console.log(rules);
+
+  for (let i = 1; i <= maxCorrectAnswers; i += 1) {
+    const question = game.question();
+    const questionPrint = `Question: ${question.string}`;
+    console.log(questionPrint);
+
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = question.answer;
+
+    const isCorrect = answer === correctAnswer;
+    if (!isCorrect) {
+      const tip = `"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`;
+      console.log(tip);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
