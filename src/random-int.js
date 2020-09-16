899b3ab6124c8fabcda098851268f0890473a20b
bontequero <bontequero@gmail.com>
2020-09-16 22:46:23 +0300

    getRandomInt now includes max value

--- src/random-int.js
+++ src/random-int.js
@@ -1,5 +1,5 @@
 export default (minp, maxp) => {
   const min = Math.ceil(minp);
   const max = Math.floor(maxp);
-  return Math.floor(Math.random() * (max - min) + min);
+  return Math.round(Math.random() * (max - min) + min);
 };
