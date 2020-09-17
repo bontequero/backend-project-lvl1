a90a868437524b032edcefb8e200332a187a4394
bontequero <bontequero@gmail.com>
2020-09-17 23:18:37 +0300

    add predicate isEven

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,12 +3,14 @@ import getRandomInt from '../random-int.js';
 const minNumber = 0;
 const maxNumber = 100;
 
+const isEven = (number) => (number % 2 === 0);
+
 export default () => {
   return {
     task: 'Answer "yes" if the number is even, otherwise answer "no".',
     question: () => {
       const number = getRandomInt(minNumber, maxNumber);
-      const answer = number % 2 === 0 ? 'yes' : 'no';
+      const answer = isEven(number) ? 'yes' : 'no';
       return {
         string: number,
         answer,
