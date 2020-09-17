fe9725fb6f9c54776ff1831091e2a93d7397f17c
bontequero <bontequero@gmail.com>
2020-09-17 23:37:08 +0300

    fix isPrime predicate

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,6 +4,9 @@ const minNumber = 0;
 const maxNumber = 100;
 
 const isPrime = (num) => {
+  if (num <= 1) {
+    return false;
+  }
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
